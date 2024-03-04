96a7cd0d269053dd53b3181e56cdb8d89b4de1ec
natalkashostak <--global>
2024-03-05 02:57:27 +0300

    added HotelManager.h

---
+++ HotelManager.h
@@ -0,0 +1,27 @@
+
+#pragma once
+
+using namespace std;
+
+#ifndef HOTELMANAGER_H
+#define HOTELMANAGER_H
+
+#include <vector>
+
+#include "HotelRoom.h"
+#include "HotelBooking.h"
+#include "HotelManager.h"
+
+class HotelManager {
+private:
+    vector<HotelBooking> bookings;
+
+public:
+    HotelManager();
+    ~HotelManager();
+
+    void addBooking(const HotelBooking& booking);
+    void displayAllBookings() const;
+};
+
+#endif /* HOTELMANAGER_H */
