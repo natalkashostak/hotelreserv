c066545eda7f5f98547144d830a8dc306a08d1cc
natalkashostak <--global>
2024-03-05 02:53:37 +0300

    added HotelBooking.h

---
+++ HotelBooking.h
@@ -0,0 +1,33 @@
+
+#pragma once
+
+#ifndef HOTELBOOKING_H
+#define HOTELBOOKING_H
+
+using namespace std;
+
+#include <string>
+#include <vector>
+
+#include "HotelRoom.h"
+#include "HotelBooking.h"
+#include "HotelManager.h"
+
+class HotelBooking {
+private:
+    string guestName;
+    int numGuests;
+    vector<HotelRoom> bookedRooms;
+
+public:
+    HotelBooking(const string& name, int guests);
+    HotelBooking();
+    ~HotelBooking();
+
+    string getGuestName() const;
+    int getNumGuests() const;
+    void addRoom(const HotelRoom& room);
+    void displayBookingInfo() const;
+};
+
+#endif /* HOTELBOOKING_H */
