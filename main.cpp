dfaaca95cd59def6dbd3537c99825a18e00e298a
natalkashostak <--global>
2024-04-15 23:33:41 +0300

    added main.cpp, and fixed files

--- main.cpp
+++ main.cpp
@@ -1,24 +1,39 @@
 
 #include <iostream>
-#include <string>
-#include <vector>
 #include "HotelRoom.h"
+#include "SuiteRoom.h"
 #include "HotelBooking.h"
 #include "HotelManager.h"
-
 using namespace std;
 
 int main() {
-    HotelRoom room1("Double", 101, 150.0);
-    HotelRoom room2("Single", 102, 100.0);
+    HotelRoom standardRoom("Standard", 101, 100.0);
+    HotelRoom doubleRoom("Double", 102, 150.0);
+    SuiteRoom suite("Presidential Suite", 201, 500.0);
 
-    HotelBooking booking1("Natalia Shostak", 2);
-    booking1.addRoom(room1);
-    booking1.addRoom(room2);
+    cout << "Room Details:" << endl;
+    standardRoom.displayInfo();
+    doubleRoom.displayInfo();
+    suite.displayInfo();
+    cout << endl;
+
+    HotelBooking booking1("John Doe", 2);
+    booking1.addRoom(&standardRoom);
+    booking1.addRoom(&doubleRoom);
+
+    HotelBooking booking2("Jane Smith", 1);
+    booking2.addRoom(&suite);
+
+    cout << "Booking Details:" << endl;
+    booking1.displayBookingInfo();
+    booking2.displayBookingInfo();
+    cout << endl;
 
     HotelManager manager;
     manager.addBooking(booking1);
+    manager.addBooking(booking2);
 
+    cout << "All Bookings:" << endl;
     manager.displayAllBookings();
 
     return 0;
