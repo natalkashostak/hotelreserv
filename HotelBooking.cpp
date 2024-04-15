d187576140f7abfd97acb97a69336e5806115cb7
natalkashostak <--global>
2024-04-15 23:35:18 +0300

    added HotelBooking.cpp, and fixed files

--- HotelBooking.cpp
+++ HotelBooking.cpp
@@ -1,31 +1,33 @@
 
-#include <iostream>
 #include "HotelBooking.h"
-
 using namespace std;
 
-HotelBooking::HotelBooking(const string& name, int guests)
-        : guestName(name), numGuests(guests) {}
+HotelBooking::HotelBooking(const string& name, int guests) : guestName(name), numGuests(guests) {
+    cout << "HotelBooking constructor called." << endl;
+}
 
-HotelBooking::HotelBooking() : guestName("Unknown"), numGuests(1) {}
-
-HotelBooking::HotelBooking(const HotelBooking& other)
-        : guestName(other.guestName), numGuests(other.numGuests), bookedRooms(other.bookedRooms) {}
+HotelBooking::HotelBooking(const HotelBooking& other) : guestName(other.guestName), numGuests(other.numGuests) {
+    for (const auto& room : other.bookedRooms) {
+        bookedRooms.push_back(room->clone()); // Use clone to copy rooms
+    }
+    cout << "HotelBooking copy constructor called." << endl;
+}
 
 HotelBooking::HotelBooking(HotelBooking&& other) noexcept
-        : guestName(move(other.guestName)), numGuests(other.numGuests), bookedRooms(move(other.bookedRooms)) {}
-
-HotelBooking::~HotelBooking() {}
-
-string HotelBooking::getGuestName() const {
-    return guestName;
+        : guestName(move(other.guestName)), numGuests(other.numGuests), bookedRooms(move(other.bookedRooms)) {
+    other.bookedRooms.clear();
+    cout << "HotelBooking move constructor called." << endl;
 }
 
-int HotelBooking::getNumGuests() const {
-    return numGuests;
+HotelBooking::~HotelBooking() {
+    cout << "HotelBooking destructor called." << endl;
+    for (auto& room : bookedRooms) {
+        delete room;  // Properly delete Room pointers to prevent memory leaks
+        room = nullptr;
+    }
 }
 
-void HotelBooking::addRoom(const HotelRoom& room) {
+void HotelBooking::addRoom(Room* room) {
     bookedRooms.push_back(room);
 }
 
@@ -34,7 +36,9 @@ void HotelBooking::displayBookingInfo() const {
     cout << "Number of Guests: " << numGuests << endl;
     cout << "Booked Rooms:\n";
     for (const auto& room : bookedRooms) {
-        room.displayRoomInfo();
+        room->displayInfo();
         cout << endl;
     }
+    cout << endl;
 }
+
