9a2090873f782c90336b27e0019a96f3633579e7
natalkashostak <--global>
2024-04-15 23:36:53 +0300

    added HotelRoom.h, and fixed files

--- HotelRoom.h
+++ HotelRoom.h
@@ -2,34 +2,26 @@
 #ifndef HOTELROOM_H
 #define HOTELROOM_H
 
-using namespace std;
-
+#include "Room.h"
 #include <iostream>
 #include <string>
+using namespace std;
 
-class HotelRoom {
+class HotelRoom : public Room {
 private:
     string roomType;
-    int roomNumber;
-    double pricePerNight;
     static int totalRooms;
 
 public:
     HotelRoom(const string& type, int number, double price);
-    HotelRoom();
     HotelRoom(const HotelRoom& other);
     HotelRoom(HotelRoom&& other) noexcept;
     ~HotelRoom();
 
-    string getRoomType() const;
-    int getRoomNumber() const;
-    double getPricePerNight() const;
+    HotelRoom& operator=(const HotelRoom& other);
+    HotelRoom* clone() const override;
+    void displayInfo() const override;
     static int getTotalRooms();
-
-    void displayRoomInfo() const;
-    HotelRoom& operator++();
-    friend istream& operator>>(istream& in, HotelRoom& room);
-    friend ostream& operator<<(ostream& out, const HotelRoom& room);
 };
 
 #endif // HOTELROOM_H
